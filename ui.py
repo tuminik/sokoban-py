fd6b24cd94998cc30eb27c2299b2b0303ea7d7e8
santiagodiazp@gmail.com <santiagodiazp@gmail.com@88f22ec3-e6f2-a780-a545-3892fab957df>
2011-07-08 21:34:12 +0000

    ya esta parece

--- ui.py
+++ ui.py
@@ -1,10 +1,10 @@
 from easygui import ccbox
 from easygui import codebox
 import Tkinter as tkinter
-import tkMessageBox
+import tkMessageBox as messagebox
 import sys
 MONOSPACE_FONT_SIZE     =  10
-tableCounter = 0
+
 
 table1 = [[' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '\n'], 
                 [' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', '\n'],
@@ -31,6 +31,10 @@ table2 = [[' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '\n'],
                 [' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', '\n']]
 
 
+class tables():
+    tabs= [table1,table2]
+    cont=0
+    
 
 def tableToStr(tableS):
     tableR=[]
@@ -38,10 +42,8 @@ def tableToStr(tableS):
         tableR.append("".join(i))
     return tableR
 
-def table():
-    
-    tableCounter=tableCounter+1
-    return
+
+
     
 
 def callmove(i):
@@ -49,6 +51,30 @@ def callmove(i):
         move(i)
     return wrap
 
+
+def printText(text,textArea):
+    try:
+        text = "".join(text)  # convert a list or a tuple to a string
+    except:
+        sys.exit(16)
+    textArea.configure(state=tkinter.NORMAL) 
+    textArea.delete(1.0, tkinter.END)
+    textArea.insert(tkinter.END,"SOKOBAN\n\n", "normal")    
+    textArea.insert(tkinter.END,text, "normal")
+    textArea.configure(state=tkinter.DISABLED)
+
+
+def changeTable(textA, tables):
+    tables.cont=+1 
+    text = tableToStr(tables.tabs[tables.cont])
+    printText(text,textA)
+    return
+
+def table(textA,  table):
+    def wrap():
+        changeTable( textA, table)
+    return wrap
+    
 TOP = "top"
 YES = True
 BOTH = "both"
@@ -61,19 +87,20 @@ screen_height = root.winfo_screenheight()
 root_width    = int((screen_width * 0.8))
 btnframe = tkinter.Frame(root)
 btnframe.pack(side=TOP, expand=YES, fill=BOTH)
-newbtn = tkinter.Button(root, command=table, text="NEXT STEP")
-newbtn.pack(side=BOTTOM, expand=NO, fill=X)
+
 character_width = int((root_width * 0.6) / MONOSPACE_FONT_SIZE)
 textArea = tkinter.Text(root,height=25,width=character_width, padx="1m", pady="1m")
 textArea.pack(side=tkinter.LEFT, fill=BOTH, expand=YES)
-text = tableToStr(table1)
-try:
-    text = "".join(text)  # convert a list or a tuple to a string
-except:
-    sys.exit(16)
-textArea.insert(tkinter.END,"SOKOBAN\n\n", "normal")    
-textArea.insert(tkinter.END,text, "normal")
-textArea.configure(state=tkinter.DISABLED)
+
+newbtn = tkinter.Button(root, command=table(textArea, tables), text="NEXT STEP")
+newbtn.pack(side=BOTTOM, expand=NO, fill=X)
+
+text = tableToStr(tables.tabs[0])
+tables.cont=+1
+printText(text,textArea)
+
+print tableCounter
+
 
 
 root.mainloop()
