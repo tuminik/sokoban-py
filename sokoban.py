e3c42ebfc767c22b204d82aa18ae43fcb1835016
santiagodiazp@gmail.com <santiagodiazp@gmail.com@88f22ec3-e6f2-a780-a545-3892fab957df>
2011-07-14 17:44:19 +0000

    heuristica funcionando, class Table agregado para que funcione el hasheo con el tipo de dato {}. heuristica funcionando falta precision. probando con ejemplo mas faciles

--- sokoban.py
+++ sokoban.py
@@ -2,6 +2,10 @@ from ai import Problem
 from ai import astar_search
 from ai import iterative_deepening_search
 from ai import depth_first_tree_search
+from ai import depth_first_graph_search
+from ai import breadth_first_tree_search
+from ai import breadth_first_graph_search
+from ai import depth_limited_search
 from tpIAsokobanparser import obtenerMapa
 import copy
 
@@ -39,81 +43,128 @@ def canMove(x, y, listMoves,  state):
         return listMoves
         
 def move(x, y,  listMoves, listStates,  state):
-    stateS = copy.deepcopy(state)
-    stateS[x][y]=' '
+    tableS = Table()
+    tableS.table= copy.deepcopy(state)
+    tableS.table[x][y]=' '
     try :
         for i in listMoves:
             if i==0:
-                stateS[x+1][y]= '@'
-                listStates.append(copy.deepcopy(stateS))     #copia la tabla y la agrega a la lista de posible movidas
-                stateS[x+1][y]= ' '                                      #"cerar" la tabla
+                tableS.table[x+1][y]= '@'
+                listStates.append(copy.deepcopy(tableS))     #copia la tabla y la agrega a la lista de posible movidas
+                tableS.table[x+1][y]= ' '                                      #"cerar" la tabla
             if i==1:
-                stateS[x-1][y]= '@'
-                listStates.append(copy.deepcopy(stateS))
-                stateS[x-1][y]= ' '
+                tableS.table[x-1][y]= '@'
+                listStates.append(copy.deepcopy(tableS))
+                tableS.table[x-1][y]= ' '
             if i==2:
-                stateS[x][y+1]= '@'
-                listStates.append(copy.deepcopy(stateS))
-                stateS[x][y+1]= ' '
+                tableS.table[x][y+1]= '@'
+                listStates.append(copy.deepcopy(tableS))
+                tableS.table[x][y+1]= ' '
             if i==3:
-                stateS[x][y-1]= '@'
-                listStates.append(copy.deepcopy(stateS))
-                stateS[x][y-1]= ' '
+                tableS.table[x][y-1]= '@'
+                listStates.append(copy.deepcopy(tableS))
+                tableS.table[x][y-1]= ' '
             if i==4:
-                stateS[x+1][y]= '@'
-                stateS[x+2][y]='$'
-                listStates.append(copy.deepcopy(stateS))
-                stateS[x+1][y]= ' '
-                stateS[x+2][y]=' '
+                tableS.table[x+1][y]= '@'
+                tableS.table[x+2][y]='$'
+                listStates.append(copy.deepcopy(tableS))
+                tableS.table[x+1][y]= ' '
+                tableS.table[x+2][y]=' '
             if i==5:
-                stateS[x-1][y]= '@'
-                stateS[x-2][y]='$'
-                listStates.append(copy.deepcopy(stateS))
-                stateS[x-1][y]= ' '
-                stateS[x-2][y]= ' '
+                tableS.table[x-1][y]= '@'
+                tableS.table[x-2][y]='$'
+                listStates.append(copy.deepcopy(tableS))
+                tableS.table[x-1][y]= ' '
+                tableS.table[x-2][y]= ' '
             if i==6:
-                stateS[x][y+1]= '@'
-                stateS[x][y+2]='$'
-                listStates.append(copy.deepcopy(stateS))
-                stateS[x][y+1]= ' '
-                stateS[x][y+2]= ' '
+                tableS.table[x][y+1]= '@'
+                tableS.table[x][y+2]='$'
+                listStates.append(copy.deepcopy(tableS))
+                tableS.table[x][y+1]= ' '
+                tableS.table[x][y+2]= ' '
             if i==7:
-                stateS[x][y-1]= '@'
-                stateS[x][y-2]='$'
-                listStates.append(copy.deepcopy(stateS))
-                stateS[x][y-1]= ' '
-                stateS[x][y-2]= ' '
+                tableS.table[x][y-1]= '@'
+                tableS.table[x][y-2]='$'
+                listStates.append(copy.deepcopy(tableS))
+                tableS.table[x][y-1]= ' '
+                tableS.table[x][y-2]= ' '
         return listStates
     except:
         return  listStates
 #funcion constructor
-class sokobanProblem(Problem):      #hereda la clase Problem de ai.py
-    def _init_(self):                                #constructor
-        self.initial=self                               #recibe la tabla de juego y construye
-    def successor(self, state):                 #funcion sucesora
-        listMoves=[]                                #lista para posibles movimientos
-        listStates=[]                               #lista para posibles estados de la tabla
-        x, y = findPlayer(state)                #encontrar donde esta el jugador
-        canMove(x, y,listMoves,  state)     #puede moverse????  
-        if not listMoves:                             #si la lista esta vacia, no hay movimientos posibles por ende no posibles sucesores
-            return []                                     #retorna lista vacia la funcion sucesor
-        else:                                                 #si la lista tiene algo!!
-            move(x, y, listMoves ,  listStates, state)      #con listMoves hago los posibles movimientos en listStates
-            new =  copy.deepcopy(state)                     #debug
+
+
+class SokobanProblem(Problem):      #hereda la clase Problem de ai.py
+    def _init_(self):
+        self.initial=self                            #recibe la tabla de juego y construye
+    def successor(self, state):                         #funcion sucesora
+        listMoves=[]                                             #lista para posibles movimientos
+        listStates=[]                                           #lista para posibles estados de la tabla
+        x, y = findPlayer(state.table)                            #encontrar donde esta el jugador
+        canMove(x, y,listMoves,  state.table)                 #puede moverse????  
+        if not listMoves:                                           #si la lista esta vacia, no hay movimientos posibles por ende no posibles sucesores
+            return []                                                   #retorna lista vacia la funcion sucesor
+        else:                                                               #si la lista tiene algo!!
+            move(x, y, listMoves ,  listStates, state.table)      #con listMoves hago los posibles movimientos en listStates
+            new =  copy.deepcopy(state.table)                     #debug
             print "-----------padre"                            
             for i in new:
                 print "".join(i)
             print "-----------finpadre"                     #debug
 #            print "-----------hijos"
 #            for i in listStates:
-#                for j in i:
+#                for j in i.table:
 #                    print "".join(j)
 #            print "-----------finhijos"
             raw_input()                                                   #debug      
             return [(moveA, wichMove(moveA, listStates, listMoves)) for moveA in listMoves] #arma una lista de pares por ejemplo [(A,B),(C,D)]
             #en este caso un par de movimiento y la tabla que se movio
-    def h(self):
-        return 1
+    
+    def h(self, node):
+        c=0
+        listBoxes=[]
+        listPlaces=[]
+        x, y = findPlayer(node.state.table)
+        listBoxes = findBoxes(listBoxes, node.state.table)
+        listPlaces = findPlaces(listPlaces, node.state.table)
+        for i in listBoxes:
+            xB, yB = i
+            c+=distancePlayerToBox(x, y, xB, yB)
+            for j in listPlaces:
+                xP, yP = j
+                c+=distancePlacesToBoxes(xP, yP, xB, yB)
+#            if i[0]+1 == pos[0]:
+#                c=+1
+#            if i[0]-1 == pos[0]:
+#                c=+1
+#            if i[1]+1 == pos[1]:
+#                c=+1
+#            if i[1]-1 == pos[1]:
+#                c=+1
+        
+        return c
+
+def findBoxes(listBoxes, state):
+    x=0
+    for i in state:
+        y=0
+        for j in i:
+            if j=='$':
+                listBoxes.append((x, y))
+            y+=1
+        x+=1
+    return listBoxes
+
+def findPlaces(listPlaces, state):
+    x=0
+    for i in state:
+        y=0
+        for j in i:
+            if j=='.':
+                listPlaces.append((x, y))
+            y+=1
+        x+=1
+    return listPlaces
 
 
 def wichMove(moveA, listStates, listMoves):
@@ -121,7 +172,7 @@ def wichMove(moveA, listStates, listMoves):
         return listStates[listMoves.index(moveA)]
     except:
         return []
-        
+
 
 def findGoalState(table):
     x=0
@@ -142,13 +193,22 @@ def findGoalState(table):
         x+=1
     return table
 
-def distPlayerToBox(state):
-    return
+def distancePlayerToBox(xPlayer, yPlayer, xBox, yBox):
+    return abs(xPlayer-xBox)+abs(yPlayer-yBox)+1
     
+    
+def distancePlacesToBoxes(xPlace, yPlace, xBox, yBox):
+    return abs(xPlace-xBox)+abs(yPlace-yBox)+5
+    
+class Table():
+    table=[]
 
-goal = obtenerMapa("workfile")
+goal = Table()
+goal.table = obtenerMapa("workfile")
 initial = copy.deepcopy(goal) #copia real
-goal = findGoalState(goal) #encontrar el estado final
-sokoban = sokobanProblem(initial, goal) 
-#depth_first_tree_search(sokoban).path()
-iterative_deepening_search(sokoban)
+goal = findGoalState(goal.table) #encontrar el estado final
+sokoban = SokobanProblem(initial, goal) 
+print astar_search(sokoban).path
+#depth_first_graph_search(sokoban)
+#depth_limited_search(sokoban)
+#iterative_deepening_search(sokoban)
